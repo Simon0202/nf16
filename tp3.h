78a40b5e45c22daa841c5251ccac214d18261bbd
LAURENT SIMON <simon.laurent@gmx.com>
2015-10-29 01:12:17 +0100

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,50 +1,63 @@
 #ifndef tp3_h
 #define tp3_h
 
-
-#endif /* tp3_h */
-
-
 //Structures
-
+typedef struct Livre T_Livre;
 struct Livre
 {
     char titre[50];
     char auteur[50];
     char edition[50];
-    int disponible;
-    struct Livre* suivant;
+    unsigned short int disponible;
+    T_Livre *suivant;
 };
 
-typedef struct Livre T_Livre;
 
+typedef struct Rayon T_Rayon;
 struct Rayon
 {
     char theme_rayon[50];
-    int nombre_livres;
-    T_Livre* premier;
-    struct Rayon* suivant;
+    unsigned short int nombre_livres;
+    T_Livre *premier;
+    T_Rayon *suivant;
 };
 
-typedef struct Rayon T_Rayon;
 
+
+typedef struct Biblio T_Biblio;
 struct Biblio
 {
     char nom[50];
-    struct Rayon* premier;
+    T_Rayon *premier;
 };
 
-typedef struct Biblio T_Biblio;
+
+
+
+
 
 //prototypes
-
 T_Livre *creerLivre (char *titre, char *auteur, char *edition);
-T_Biblio *creerBiblio (char *nom);
 T_Rayon *creerRayon(char *theme);
+T_Biblio *creerBiblio (char *nom);
+
 int ajouterLivre(T_Rayon *rayon, T_Livre *livre);
+int ajouterRayon(T_Biblio *biblio, T_Rayon *rayon);
+void afficherBiblio(T_Biblio *biblio);
+void afficherRayon(T_Rayon *rayon);
 int emprunterLivre(T_Rayon *rayon, char* titre);
 int supprimerLivre(T_Rayon *rayon, char* titre);
-void rechercherLivres(T_Biblio *biblio, char* critereTitre);
+void supprimerRayon(T_Biblio *biblio, char *nom_rayon);
 void trierTableau(char ***tab, int tailleTab);
+void rechercherLivres(T_Biblio *biblio, char* critereTitre);
+
+
+
+
+#endif /* tp3_h */
+
+
+
+
 
 
